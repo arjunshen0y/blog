07f4a59f4754264d7e70529f6f44e2c0c33febaf
Arjun Shenoy <arjunshenoy23@gmail.com>
2023-09-14 21:12:55 +0530

    The project link was missing. Added Ycombinator link

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,7 +3,7 @@ const projectsData = [
     title: 'Stay Tuned!!',
     imgSrc: '/static/images/dino.png',
     description: 'Projects until now are in my Resume. Please check it!',
-    href: 'www.google.com'
+    href: 'https://news.ycombinator.com/'
     
   },
 ]
