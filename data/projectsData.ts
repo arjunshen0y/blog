f719e3807ea8f1d42c2ea775753df7d79ef8f9e1
Arjun Shenoy <arjunshenoy23@gmail.com>
2023-09-14 17:59:38 +0530

    Initial Deployment Changes

---
+++ data/projectsData.ts
@@ -0,0 +1,10 @@
+const projectsData = [
+  {
+    title: 'Stay Tuned!!',
+    imgSrc: '/static/images/dino.png',
+    description: 'Projects until now are in my Resume. Please check it!'
+    
+  },
+]
+
+export default projectsData
